0e2a55b6cc86d3bbfc1a854aa145aa65dc852e6e
caguilerapavez <caguilerapavez@icloud.com>
2023-08-16 10:21:03 -0400

    segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'shows/index'
+  get 'shows/create'
+  get 'shows/new'
+  get 'documentaryfilms/index'
+  get 'documentaryfilms/create'
+  get 'documentaryfilms/new'
+  get 'movies/index'
+  get 'movies/create'
+  get 'movies/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/shows_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ShowsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get shows_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get shows_create_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get shows_new_url
+    assert_response :success
+  end
+end
